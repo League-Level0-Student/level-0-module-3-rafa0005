ff1fb6ce0b081d1199e584489487a1e721e86a8c
league <league@wts14-3.attlocal.net>
2024-10-12 12:57:08 -0700

    today i did some else if

--- src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
+++ src/_01_else_if/_2_crazy_cat_lady/CrazyCatLady.java
@@ -3,17 +3,31 @@ package _01_else_if._2_crazy_cat_lady;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user how many cats they have
+		String dogs = JOptionPane.showInputDialog("how many dogs do you have");
 		// 2. Convert their answer into an int
+		int cats = Integer.parseInt(dogs);
 		// 3. If they have 3 or more cats, tell them they are a crazy cat lady
+		if(cats  >=3) {
+			JOptionPane.showMessageDialog(null, "you are a crazy dog lady");
+		}
+		
+		if(cats >0 && cats <3) {
+			playVideo("https://www.instagram.com/evedsouza/reel/Cy3kpP0S_Qa/?hl=en");
+		}
 		// 4. If they have less than 3 cats AND more than 0 cats, call the method below to show them a cat video
+		
 		//    Hint: You can use the && operator 
 		// 5. If they have 0 cats, show them a video of A Frog Sitting on a Bench Like a Human
 		
-		
+		if(cats == 0) {
+			playVideo("https://www.youtube.com/watch?v=oj_yLBltPE8");
+		}
 		
 		
 		
--- src/_01_else_if/_3_high_low/HighLowGame.java
+++ src/_01_else_if/_3_high_low/HighLowGame.java
@@ -3,20 +3,22 @@ package _01_else_if._3_high_low;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class HighLowGame {
 
 	public static void main(String[] args) {
 		// 3. Change this line to give you a random number between 1 - 100. 
-		int random = new Random().nextInt(5);
+		int random = new Random().nextInt(101);
 		
 		// 2. Print out the random variable above
-		
+		System.out.println(random);
 		// 11. Repeat steps 1 to 10 ten times
 		
 			// 1. Ask the user for a guess using a pop-up window, and save their response 
-
+			String save = JOptionPane.showInputDialog("Guess a number");
 			// 4. Convert the users’ answer to an int (Integer.parseInt(string))
-			
+			int label = Integer.parseInt(save);
 			// 5. if the guess is correct
 				// 6. Win
 				// 12. Use "System.exit(0);" to quit the game if the user guessed the right answer.
