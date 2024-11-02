8cff2704abc109c73e9137d73b4359edf9081695
league <league@imac-24.attlocal.net>
2024-11-02 12:51:02 -0700

    today i did not get much done

--- src/_02_loop_variables/_1_years_alive/yearsalive.java
+++ src/_02_loop_variables/_1_years_alive/yearsalive.java
@@ -1,5 +1,12 @@
 package _02_loop_variables._1_years_alive;
 
-public class yearsalive {
+import javax.swing.JOptionPane;
 
+public class yearsalive {
+public static void main(String[] args) {
+	for(int i = 2025; i < (2025); i++) {
+		 JOptionPane.showMessageDialog(null, "2012");
+		 
+}
+}
 }
