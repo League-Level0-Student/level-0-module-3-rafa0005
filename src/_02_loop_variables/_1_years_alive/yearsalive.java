5569b5e30031fd753473e740e60d6ff3ad4c5095
league <league@imac-34.attlocal.net>
2024-11-09 12:57:09 -0800

    i am halfway through loop variables!

--- src/_02_loop_variables/_1_years_alive/yearsalive.java
+++ src/_02_loop_variables/_1_years_alive/yearsalive.java
@@ -1,11 +1,15 @@
 package _02_loop_variables._1_years_alive;
 
-import javax.swing.JOptionPane;
+
 
 public class yearsalive {
 public static void main(String[] args) {
-	for(int i = 2025; i < (2025); i++) {
-		 JOptionPane.showMessageDialog(null, "2012");
+	for(int i = 2012; i < (2025); i++) {
+System.out.println(i);
+		
+		
+
+
 		 
 }
 }
--- src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
+++ src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
@@ -9,9 +9,10 @@ public class WalkOfFame {
 		
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
-
+		rob.setX(100);
+		rob.penDown;
 		// 2. Make the robot draw a star shape. Hint: angle=144.
-
+ rob.setAngle(144);
 		// 3. Set the length of each line in the star to 30.
 
 		/** THE CHALLENGE: **/
