cc4eb3b1126ae1e3f60451440e469154b3cf01b9
league <maddoxseul@sbsd.net>
2024-11-23 12:52:51 -0800

    today i made a card for my mom

---
+++ src/birthday46.java
@@ -0,0 +1,26 @@
+
+
+public class birthday46 {
+
+	public birthday46() {
+	
+}
+	
+		public static void main(String[] args) {
+			size(1000, 1000);
+		}
+
+		private static void size(int i, int j) {
+			
+			ellipse(200, 500, 400, 400);
+		}
+
+		private static void ellipse(int i, int j, int k, int l) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	
+	}
+
+
